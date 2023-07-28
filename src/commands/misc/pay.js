63bb70398f007b58f3299e39c08fdac6029678f8
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-07-28 14:14:45 +0530

    Update pay.js

--- src/commands/misc/pay.js
+++ src/commands/misc/pay.js
@@ -1,4 +1,4 @@
-/**
+ /**
  * @type {import("@structures/Command")}
  */
 module.exports = {
@@ -26,6 +26,7 @@ module.exports = {
       choices:[{
         name:`Prithvi`,value:`Prithvi`},
                {name:`Icy`,value:`Icy`},
+               {name:`Sanchit`,value:`Sanchit`},
                {name:`Tanki`,value:`Tanki`}]}],
   },
 
@@ -40,17 +41,18 @@ module.exports = {
     if (user == 'Prithvi'){
       var link = 'https://www.paypal.me/prithvirajghorpade'
       var p = '8806661777'}
-    else if (use == 'Tanki'){
-  var link = 'https://www.paypal.me/tanki667'
-  var p = '9534269526@paytm'}
-else if (a[2].value == 'Icy'){
-  var link = 'https://www.paypal.me/icyalfred'
-  var p = '9534269526@paytm'
+    else if (user == 'Tanki'){
+      var link = 'https://www.paypal.me/tanki667'
+      var p = '9050661960@paytm'}
+    else if (user == 'Icy'){
+      var link = 'https://www.paypal.me/icyalfred'
+      var p = '9050661960@paytm'
   }
-else {
-  var link = 'https://www.paypal.me/Rinkiinterprices'
-  var p = '9534269526@paytm'
+    else {
+      var link = 'https://www.paypal.me/Rinkiinterprices'
+      var p = '9050661960@paytm'
   }
-    await interaction.followUp(``);
+    await interaction.followUp(`As per our company policy, payment have to be done 1st before we can deliver the product\n\nItems ordered :${order}\n\nPrice:${cost}\n\nPls send the payment to :\n[Paypal](${link}) or Paytm : ${p}\n\nDrop us a screenshot after sending, thanks\n\nRegards,\nDesign Wonderland Management`
+                               `);
   },
 };
