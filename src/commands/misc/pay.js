564c7ff96c9f2c13fd827f84f794fa48609e8492
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-07-28 14:11:03 +0530

    Update pay.js

--- src/commands/misc/pay.js
+++ src/commands/misc/pay.js
@@ -1,1 +1,56 @@
+/**
+ * @type {import("@structures/Command")}
+ */
+module.exports = {
+  name: "pay",
+  description: "shows the payment information",
+  category: "MISC",
+  command: {
+    enabled: false,
+  },
+  slashCommand: {
+    enabled: true,
+    ephemeral: true,
+    options: [{
+      name : `order`,
+      description:`Items ordered`,
+      required:true},
+              {
+      name : `cost`,
+      description:` Price of Item`,
+      required:true},
+              {
+      name:`user`,
+      description:`Choose the user`,
+      required:true,
+      choices:[{
+        name:`Prithvi`,value:`Prithvi`},
+               {name:`Icy`,value:`Icy`},
+               {name:`Tanki`,value:`Tanki`}]}],
+  },
 
+  /*async messageRun(message, args) {
+    await message.safeReply(`🏓 Pong : \`${Math.floor(message.client.ws.ping)}ms\``);
+  },*/
+
+  async interactionRun(interaction) {
+    let order = interaction.getStringOption('order')
+    let cost = interaction.getStringOption('cost')
+    let user = interaction.getStringOption('user')
+    if (user == 'Prithvi'){
+      var link = 'https://www.paypal.me/prithvirajghorpade'
+      var p = '8806661777'}
+    else if (use == 'Tanki'){
+  var link = 'https://www.paypal.me/tanki667'
+  var p = '9534269526@paytm'}
+else if (a[2].value == 'Icy'){
+  var link = 'https://www.paypal.me/icyalfred'
+  var p = '9534269526@paytm'
+  }
+else {
+  var link = 'https://www.paypal.me/Rinkiinterprices'
+  var p = '9534269526@paytm'
+  }
+    await interaction.followUp(``);
+  },
+};
